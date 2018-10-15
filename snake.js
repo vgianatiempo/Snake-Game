82ec8ce6f7f9b3b43c6d24a79ee158f759f14af0
vgianatiempo <43014662+vgianatiempo@users.noreply.github.com>
2018-10-15 16:14:53 -0400

    assignment 2

--- snake.js
+++ snake.js
@@ -1,18 +1,68 @@
-class Snake{
-   constructor() {
-    this.Position_ = 0;
-    this.direction_= "up"
+class Point {
+  constructor(x,y){
+    this.x_=x
+    this.y_=y
   }
-  move () {
-  console.log("The snake moves....");
-    this.Position_ = this.Position_ + 2;
+  get x() {
+    return this.x_;
   }
-  turn () {
-    if (this.direction_ == "down") this.direction_= "up"
-    else this.direction_="down"
+  get y() {
+    return this.y_;
   }
 }
-let bigSnake = new Snake("big")
-bigSnake.move()
-bigSnake.move();
-console.log("Big snake moved a total of:", bigSnake.totalSquares_, "Squares");
+class Snake{
+   constructor() {
+    this.position_ = new Point(0,0);
+    this.direction_= "left"
+  }
+  move (numSquares) {
+    console.log("The snake moves...");
+    if (this.direction_=="right") this.position_= new Point(this.position_.x + numSquares, this.position_.y) 
+    else if (this.direction_=="down") this.position_= new Point (this.position_.x,this.position_.y - numSquares)
+    else if (this.position_=="left") this.position_= new Point(this.position_.x - numSquares, this.position_.y)
+    else if (this.position_=="up") this.position_= new Point(this.position_.x, this.position_.y + numSquares)
+    
+  }
+  turnLeft() {
+    if (this.direction_=="right") this.direction_="up"
+    else if (this.direction_=="up") this.direction_="left"
+    else if (this.direction_=="left") this.direction_="down"
+    else if (this.direction_=="down") this.direction_="right"
+  }
+  turnRight () {
+    if (this.direction_=="right") this.direction_="down"
+    else if (this.direction_=="down") this.direction_="left"
+    else if (this.direction_=="left") this.direction_="up"
+    else if (this.direction_=="up") this.direction_="right"
+  }
+  get position() {
+   return this.position_;
+  }
+  get direction() {
+    return this.direction_;
+  }
+}
+class WorldModel {
+  constructor (s){
+    if(s instanceof Snake) this.snake_= s;
+    else throw new Error("Not given a valid snake");
+  }
+  update(steps) {
+    this.snake_.move(steps);
+  }
+  get snake() {
+    return this.snake_;
+  }
+}
+let bigSnake = new Snake()
+let worldModel= new WorldModel(bigSnake)
+worldModel.update(4)
+console.log("Big snakes new position is:", bigSnake.position,);
+bigSnake.turnLeft();
+console.log("Big snake turns:", bigSnake.direction,);
+bigSnake.move(2);
+console.log("Big snake new position is:", bigSnake.position,);
+bigSnake.turnLeft();
+console.log("Big snake turns:", bigSnake.direction,);
+bigSnake.move(4);
+console.log("Big snake new position is:", bigSnake.position,);
